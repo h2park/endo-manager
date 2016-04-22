d59de17f512d023614dcf828ed1008c08c391c9a
sqrt <sqrt@octoblu.com>
2016-04-22 10:55:33 -0700

    Do it

--- src/containers/home.js
+++ src/containers/home.js
@@ -3,16 +3,37 @@ import superagent from 'superagent'
 import url from 'url'
 
 class Home extends Component {
-  componentWillMount() {
+  componentDidMount(){
     const {credentialsDeviceUrl,meshbluBearer} = url.parse(location.href, true).query
+    this.setState({credentialsDeviceUrl,meshbluBearer})
+  }
+
+  componentWillMount() {
+    // superagent
+    //   .get(`${credentialsDeviceUrl}/user-devices`)
+    //   .set('Authorization', `Bearer ${meshbluBearer}`)
+    //   .end()
+  }
+
+  doIt(event) {
+    event.preventDefault()
+    const {credentialsDeviceUrl,meshbluBearer} = this.state
 
     superagent
-      .get(`${credentialsDeviceUrl}/user-devices`)
+      .post(`${credentialsDeviceUrl}/user-devices`)
       .set('Authorization', `Bearer ${meshbluBearer}`)
       .end()
   }
+
   render() {
-    return <h2>Home Page</h2>
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <form>
+          <button onClick={this.doIt.bind(this)} type="submit">Do it</button>
+        </form>
+      </div>
+    )
   }
 }
 
