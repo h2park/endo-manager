c79a9fc5fc22173fa1c08303cb3a371f17fdc5e0
sqrt <sqrt@octoblu.com>
2016-05-02 17:57:33 -0700

    Sliiddde that button on over

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -5,8 +5,8 @@ import {Button} from 'zooid-ui'
 const Actions = ({}) => {
   return (
     <div className={styles.actions}>
-      <Button kind="no-style">Cancel</Button>
-      <Button kind="primary">Do it</Button>
+      <Button kind="no-style" size="large">Cancel</Button>
+      <Button kind="primary"  size="large">Do it</Button>
     </div>
   )
 }
