b9d482dd81988e3120b599a70eefd7afee90b468
Roy van de Water <roy.vwater@gmail.com>
2016-06-01 16:04:03 -0700

    v1.1.2 Fix cloudfront id

--- deploy/invalidate-cloudfront.sh
+++ deploy/invalidate-cloudfront.sh
@@ -17,7 +17,7 @@ configure_awscli(){
 create_invalidation(){
   local git_tag=$1
   aws cloudfront create-invalidation \
-    --distribution-id E29V3COHX0ZSK7 \
+    --distribution-id E1WFIRDB3H5M3O \
     --invalidation-batch "{\"Paths\": {\"Quantity\": 3, \"Items\": [\"/index.html\", \"/bundle.js\", \"/bundle.js.map\"]}, \"CallerReference\": \"travis-$git_tag\"}"
 }
 
